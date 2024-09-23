baea0c15df4840e7785684e9a6799f7f494d27cb
Tcroneb Hackx <cartoonboyzfamily@gmail.com>
2024-09-23 14:52:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const settingsMenu = [
   ]
 DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
-if (!(process.env.SESSION || process.env.SESSION_ID)) throw new Error("No session found, add session before starting bot")
+if (!(process.env.SESSION || "Raganork~f027c36514e6ef5ff8d7fa3ae8bedf9c:5453775a34374c356d516f3d", throw new Error("Raganork~f027c36514e6ef5ff8d7fa3ae8bedf9c:5453775a34374c356d516f3d")
 module.exports = {
     VERSION: 'v4.0.0',
     ALIVE: process.env.ALIVE || "https://i.imgur.com/KCnoMM2.jpg Hey {sender}, I'm alive \n Uptime: {uptime}",
